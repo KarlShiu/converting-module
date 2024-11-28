aeb6d85572dfe2ae75348b5532a86606e8e8353b
Karl Shiu <bkshiu1@myseneca.ca>
2024-11-27 23:32:53 -0500

    Initial commit

---
+++ converting.c
@@ -0,0 +1,24 @@
+// CONVERTING MODULE SOURCE
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 80
+#include "converting.h"
+
+// V1: Convert string to integer
+void converting(void) {
+    printf("*** Start of Converting Strings to int Demo ***\n");
+    char intString[BUFFER_SIZE];
+    int intNumber;
+
+    do {
+        printf("Type an int numeric string (q - to quit):\n");
+        fgets(intString, BUFFER_SIZE, stdin);
+        intString[strlen(intString) - 1] = '\0'; // Remove newline character
+
+        if (strcmp(intString, "q") != 0) {
+            intNumber = atoi(intString); // Convert string to integer
+            printf("Converted number is %d\n", intNumber);
+        }
+    } while (strcmp(intString, "q") != 0);
+
+    printf("*** End of Converting Strings to int Demo ***\n");
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "converting.h"
+
+int main(void) {
+    converting(); // Call the converting function
+    return 0;
+}
